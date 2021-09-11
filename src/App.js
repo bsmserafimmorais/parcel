8b1d06c9cd31153ffcaeddabe2cfb64c083886f1
bruno serafim de morais <bsmserafim@outlook.com>
2021-09-11 16:42:38 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { listenEvent } from '@i2020/utils'
+
+const App = () => {
+  const [tasks, updateTasks] = useState([])
+
+  useEffect(() => {
+    window.addEventListener('@i2020/react-route/todo/add-task', event => {
+      updateTasks(oldTasks => [
+        ...oldTasks,
+        event.detail,
+      ])
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>@i2020/parcel</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map(task => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.describe}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
